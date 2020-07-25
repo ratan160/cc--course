eaf520d989acf748970ec29ec854587e3fb56d74
ratan160 <56647255+ratan160@users.noreply.github.com>
2020-07-25 16:58:14 +0530

    Add files via upload

---
+++ tut4b.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout<<"Tell me your age"<<endl;
+    cin>>age;
+    switch(age)
+    {
+        case 18:
+        cout<<"you are 18"<<endl;
+        break;
+        case 22:
+        cout<<"you are 22"<<endl;
+        break;
+        case 2:
+        cout<<"you are 2"<<endl;
+        break;
+
+        default:
+        cout<<"no special cases"<<endl;
+        break;
+    }
+        cout<<" Done with switch case";
+        return 0;
+}
